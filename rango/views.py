0932ed7d5fafed62225f495a58bf12a984f74627
Chieh Teng <2339910t@student.gla.ac.uk>
2018-01-31 01:31:59 +0000

    update 4.4 exercise

--- rango/views.py
+++ rango/views.py
@@ -1,11 +1,11 @@
 from django.shortcuts import render
-
 from django.http import HttpResponse
 
 def index(request):
-#return HttpResponse("Rango says hey there partner!")
+    #return HttpResponse("Rango says hey there partner!")
     context_dict = {'boldmessage': "Crunchy, creamy, cookie, candy, cupcake!"}
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
-    return HttpResponse("Rango sys here is the about page.")
+    #return HttpResponse("Rango sys here is the about page.")
+    return render(request, 'rango/about.html')
